381bb94bcadd5a5e8ea5f3739a5873f7c02eb995
Ivo Lejon <ivo.lejon@gmail.com>
2018-11-29 14:57:13 +0100

    small update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#sudo git clone https://github.com/aidhedge/monitor
+
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ubuntu
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/aidhedge/monitor .
 sudo touch .env
